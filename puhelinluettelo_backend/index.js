2a1333478fef0c57569d353fb1404c1c67fb0c01
jussilah <heli.jussila@cgi.com>
2018-10-28 08:11:14 +0200

    3.16-3.18 done.

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -19,72 +19,93 @@ app.use(bodyParser.json())
 morgan.token('request', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :request :status :res[content-length] - :response-time ms'))
 
+app.get('/info', (req, res) => {
+    Person
+        .find({})
+        .then(p => {
+            res.send(`<p>puhelinluettelossa on ${p.length} henkilön tiedot<br></br>${new Date()}</p>`)
+        })
+
+})
+
 app.get('/api/persons', (req, res) => {
     Person
         .find({})
         .then(p => {
             res.json(p.map(formatPerson))
+        }).catch(error => {
+            console.log(error)
+            response.status(404).end()
         })
 })
-
-app.get('/info', (req, res) => {
-    Person
-    .find({})
-    .then(p => {
-        res.send(`<p>puhelinluettelossa on ${p.length} henkilön tiedot<br></br>${new Date()}</p>`)
-    })
-    
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    Person
-        .findByIdAndRemove(request.params.id) .then(person => {
-            response.status(204).end()
-        })
-    
-})
-
 
 app.get('/api/persons/:id', (request, response) => {
     Person
         .findById(request.params.id)
         .then(person => {
-            response.json(formatPerson(person))
+            if (person) {
+                response.json(formatPerson(person))
+            } else {
+                response.status(404).end()
+            }
+        }).catch(error => {
+            console.log(error)
+            response.status(400).send({ error: 'malformatted id' })
         })
-    /*if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }*/
 })
+
+app.put('/api/persons/:id', (request, response) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number
+    }
+  
+    Person
+      .findByIdAndUpdate(request.params.id, person, { new: true } )
+      .then(updatedNote => {
+        response.json(formatPerson(updatedNote))
+      })
+      .catch(error => {
+        console.log(error)
+        response.status(400).send({ error: 'malformatted id' })
+      })
+  })
 
 app.post('/api/persons/', (request, response) => {
     const body = request.body
 
     if (body.name === undefined) {
-        return response.status(400).json({ error: 'name missing' })
+        response.status(400).send({ error: 'name missing' })
     }
     if (body.number === undefined) {
-        return response.status(400).json({ error: 'number missing' })
+        response.status(400).send({ error: 'number missing' })
     }
-    Person
-        .find({})
-        .then(p => {
-            if (p.filter(p => p.name === body.name).length > 0) {
-                return response.status(400).json({ error: 'name must be unique' })
-            }
-        })
 
     const person = new Person({
         name: body.name,
-        number: body.number,
+        number: body.number
     })
 
     person
-        .save()
-        .then(savedPerson => {
-            response.json(formatPerson(savedPerson))
+    .save()
+    .then(savedPerson => {
+      return formatPerson(savedPerson)
+    })
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
+    })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    Person
+        .findByIdAndRemove(request.params.id).then(person => {
+            response.status(204).end()
+        }).catch(error => {
+            response.status(400).send({ error: 'malformatted id' })
         })
+
 })
 
 const PORT = process.env.PORT || 3001
